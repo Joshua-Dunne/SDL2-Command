fd1302bc6cec71d4b2e75869f7d2433ad803683b
Joshua-Dunne <c00241588@itcarlow.ie>
2021-10-20 13:03:42 +0100

    Fully implemented Command Pattern. Implemented clickable buttons.

--- include/Game.h
+++ include/Game.h
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 #include <string>
+#include "TextureData.h"
 #include "MacroCommand.h"
 #include "LegoCommand.h"
 #include "ClayCommand.h"
@@ -19,10 +20,15 @@ private:
 
     //Loads media
     void loadMedia();
+
+    SDL_Texture* loadFromFile(std::string path, SDL_Texture* tex, TextureData& data);
+    void renderTexture(SDL_Texture* t_tex, TextureData t_data);  
+
     //Frees media and shuts down SDL
     void close();
 
     void processEvents();
+    void processMouse(SDL_MouseButtonEvent& b);
     void update();
     void render();
 
@@ -34,17 +40,38 @@ private:
     SDL_Event e;
 
     //Screen dimension constants
-    const int SCREEN_WIDTH = 640;
-    const int SCREEN_HEIGHT = 480;
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 512;
 
     //The window we'll be rendering to
     SDL_Window* window = NULL;
 
-    //The surface contained by the window
+     //The surface contained by the window
     SDL_Surface* screenSurface = NULL;
 
-    //The image we will load and show on the screen
-    SDL_Surface* gHelloWorld = NULL;
+    // Renders stuff idk?
+    SDL_Renderer* renderer = NULL;
+
+    SDL_Texture* legoButton = NULL;
+    TextureData legoButtonData;
+
+    SDL_Texture* clayButton = NULL;
+    TextureData clayButtonData;
+
+    SDL_Texture* concreteButton = NULL;
+    TextureData concreteButtonData;
+
+    SDL_Texture* timberButton = NULL;
+    TextureData timberButtonData;
+
+    SDL_Texture* undoButton = NULL;
+    TextureData undoData;
+
+    SDL_Texture* redoButton = NULL;
+    TextureData redoData;
+
+    SDL_Texture* executeButton = NULL;
+    TextureData executeButtonData;
 
     MacroCommand macro;
 
--- src/Game.cpp
+++ src/Game.cpp
@@ -2,17 +2,7 @@
 
 Game::Game() :
     m_gameIsRunning{ false }
-{
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-    }
-    else
-    {
-        //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-    }
+{  
 }
 
 Game::~Game()
@@ -39,36 +29,11 @@ void Game::run()
         {
             //Get window surface
             screenSurface = SDL_GetWindowSurface( window );
+            renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
         }
     }
 
     loadMedia();
-
-    MacroCommand macro;
-
-    macro.add(new LegoCommand());
-    macro.add(new TimberCommand());
-
-    macro.execute();
-
-    std::cin.get();
-
-    macro.undo();
-    macro.execute();
-
-    std::cin.get();
-
-    macro.add(new LegoCommand());
-    macro.add(new TimberCommand());
-
-    macro.execute();
-
-    std::cin.get();
-
-    macro.redo();
-    macro.execute();
-
-    std::cin.get();
 
     m_gameIsRunning = true;
     while (m_gameIsRunning)
@@ -81,12 +46,71 @@ void Game::run()
 
 void Game::loadMedia()
 {
-    //Load splash image
-    gHelloWorld = SDL_LoadBMP( "../resources/images/legobr.bmp" );
-    if( gHelloWorld == NULL )
+    legoButton = loadFromFile("../resources/images/legobr.bmp", legoButton, legoButtonData);
+    legoButtonData.x = 0;
+    legoButtonData.y = 0;
+
+    clayButton = loadFromFile("../resources/images/claybr.bmp", clayButton, clayButtonData);
+    clayButtonData.x = 256;
+    clayButtonData.y = 0;
+
+    concreteButton = loadFromFile("../resources/images/concretebr.bmp", concreteButton, concreteButtonData);
+    concreteButtonData.x = 0;
+    concreteButtonData.y = 256;
+
+    timberButton = loadFromFile("../resources/images/timberbr.bmp", timberButton, timberButtonData);
+    timberButtonData.x = 256;
+    timberButtonData.y = 256;
+    
+    undoButton = loadFromFile("../resources/images/undo.bmp", undoButton, undoData);
+    undoData.x = 256 + undoData.width * 2.0;
+    undoData.y = 0;
+
+    redoButton = loadFromFile("../resources/images/redo.bmp", redoButton, redoData);
+    redoData.x = 256 + redoData.width * 2.0;
+    redoData.y = redoData.height;
+
+    executeButton = loadFromFile("../resources/images/execute.bmp", executeButton, executeButtonData);
+    executeButtonData.x = 256 + executeButtonData.width * 2.0;
+    executeButtonData.y = executeButtonData.height * 2;
+}
+
+SDL_Texture* Game::loadFromFile(std::string path, SDL_Texture* tex, TextureData& data)
+{
+    //Load image at specified path
+    SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
+
+    if (loadedSurface == NULL)
     {
-        printf( "Unable to load image %s! SDL Error: %s\n", "resources/images/legobr.bmp", SDL_GetError() );
+        printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), SDL_GetError());
     }
+    else
+    {
+        std::cout << "loadedSurface set :)" << std::endl;
+        //Color key image
+        SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
+
+        //Create texture from surface pixels
+        tex = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+
+
+        if (tex == NULL)
+        {
+            std::cout << "Unable to create texture from %s! SDL Error: %s\n" << path.c_str() << SDL_GetError() << std::endl;
+        }
+        else
+        {
+            std::cout << "tex loaded :)" << std::endl;
+            //Get image dimensions
+            data.width = loadedSurface->w;
+            data.height = loadedSurface->h;
+        }
+
+        //Get rid of old loaded surface
+        SDL_FreeSurface(loadedSurface);
+    }
+
+    return tex;
 }
 
 void Game::processEvents()
@@ -100,12 +124,92 @@ void Game::processEvents()
         {
             m_gameIsRunning = false;
         }
+
+        if(e.type == SDL_MOUSEBUTTONDOWN)
+        {
+            processMouse(e.button);
+        }
     }
+}
+
+void Game::processMouse(SDL_MouseButtonEvent& b)
+{
+    if(b.button == SDL_BUTTON_LEFT){
+    //handle a left-click
+        if(b.x >= legoButtonData.x && b.x <= legoButtonData.x + legoButtonData.width)
+        {
+            if(b.y >= legoButtonData.y && b.y <= legoButtonData.y + legoButtonData.height)
+            {
+                macro.add(new LegoCommand());
+            }
+        }
+
+        if(b.x >= clayButtonData.x && b.x <= clayButtonData.x + clayButtonData.width)
+        {
+            if(b.y >= clayButtonData.y && b.y <= clayButtonData.y + clayButtonData.height)
+            {
+                macro.add(new ClayCommand());
+            }
+        }
+
+        if(b.x >= concreteButtonData.x && b.x <= concreteButtonData.x + concreteButtonData.width)
+        {
+            if(b.y >= concreteButtonData.y && b.y <= concreteButtonData.y + concreteButtonData.height)
+            {
+                macro.add(new ConcreteCommand());
+            }
+        }
+
+        if(b.x >= timberButtonData.x && b.x <= timberButtonData.x + timberButtonData.width)
+        {
+            if(b.y >= timberButtonData.y && b.y <= timberButtonData.y + timberButtonData.height)
+            {
+                macro.add(new TimberCommand());
+            }
+        }
+
+        if(b.x >= undoData.x && b.x <= undoData.x + undoData.width)
+        {
+            if(b.y >= undoData.y && b.y <= undoData.y + undoData.height)
+            {
+                macro.undo();
+            }
+        }
+
+        if(b.x >= redoData.x && b.x <= redoData.x + redoData.width)
+        {
+            if(b.y >= redoData.y && b.y <= redoData.y + redoData.height)
+            {
+                macro.redo();
+            }
+        }
+
+        if(b.x >= executeButtonData.x && b.x <= executeButtonData.x + executeButtonData.width)
+        {
+            if(b.y >= executeButtonData.y && b.y <= executeButtonData.y + executeButtonData.height)
+            {
+                macro.execute();
+            }
+        }
+  }
 }
 
 void Game::update()
 {
    // std::cout << "Updating" << std::endl;
+}
+
+void Game::renderTexture(SDL_Texture* t_tex, TextureData t_data)
+{
+    //Set rendering space and render to screen
+    SDL_Rect renderQuad;
+    renderQuad.x = t_data.x;
+    renderQuad.y = t_data.y;
+    renderQuad.w = t_data.width;
+    renderQuad.h = t_data.height;
+    
+    //Render to screen
+    SDL_RenderCopy(renderer, t_tex, NULL, &renderQuad);
 }
 
 void Game::render()
@@ -118,17 +222,25 @@ void Game::render()
     }
     else
     {
-        //Get window surface
-        screenSurface = SDL_GetWindowSurface( window );
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-        //Fill the surface white
-        SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+        SDL_RenderClear(renderer);
 
-        //Apply the image
-        SDL_BlitSurface( gHelloWorld, NULL, screenSurface, NULL );
+        renderTexture(legoButton, legoButtonData);
 
-        //Update the surface
-        SDL_UpdateWindowSurface( window );
+        renderTexture(clayButton, clayButtonData);
+
+        renderTexture(concreteButton, concreteButtonData);
+
+        renderTexture(timberButton, timberButtonData);
+
+        renderTexture(undoButton, undoData);
+
+        renderTexture(redoButton, redoData);
+
+        renderTexture(executeButton, executeButtonData);
+
+        SDL_RenderPresent(renderer);
     }
 
 
@@ -136,11 +248,7 @@ void Game::render()
 
 void Game::cleanUp()
 {
-    std::cout << "Cleaning up" << std::endl;
-
-    //Deallocate surface
-    SDL_FreeSurface( gHelloWorld );
-    gHelloWorld = NULL;
+    std::cout << "Cleaning up" << std::endl;    
 
     //Destroy window
     SDL_DestroyWindow( window );
